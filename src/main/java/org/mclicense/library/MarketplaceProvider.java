fce32d4dfefeeb009a53f777d6290de87152fddd
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2025-01-18 13:16:45 +0000

    add spgiot to MarketplaceProvider

--- src/main/java/org/mclicense/library/MarketplaceProvider.java
+++ src/main/java/org/mclicense/library/MarketplaceProvider.java
@@ -5,14 +5,14 @@ public class MarketplaceProvider {
     private static String pmPlaceholder = "%%__POLYMART__%%";
     private static String pmLicense = "%%__LICENSE__%%";
 
-    private static String spUser = "%%__USER__%";
     private static String spResource = "%%__RESOURCE__%";
+    private static String spUser = "%%__USER__%";
 
     protected static String getHardcodedLicense() {
         if (pmPlaceholder.equals("1") && !pmLicense.startsWith("%%__")) {
             return "pm_" + pmLicense;
-        } else if (!spUser.startsWith("%%__") && !spResource.startsWith("%%__")) {
-            return "sp_" + spResource + "_" + spUser;
+        } else if (!spResource.startsWith("%%__") && !spUser.startsWith("%%__")) {
+            return "spraw_" + spResource + "_" + spUser;
         }
         return null;
     }
