ccf4c5923d0604c97b786f25c411b1d4ff60c116
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2025-01-18 13:12:45 +0000

    add spgiot to MarketplaceProvider

--- src/main/java/org/mclicense/library/MarketplaceProvider.java
+++ src/main/java/org/mclicense/library/MarketplaceProvider.java
@@ -1,13 +1,18 @@
 package org.mclicense.library;
 
-// Supported marketplaces: Polymart
+// Supported marketplaces: Polymart, Spigot
 public class MarketplaceProvider {
     private static String pmPlaceholder = "%%__POLYMART__%%";
     private static String pmLicense = "%%__LICENSE__%%";
 
+    private static String spUser = "%%__USER__%";
+    private static String spResource = "%%__RESOURCE__%";
+
     protected static String getHardcodedLicense() {
         if (pmPlaceholder.equals("1") && !pmLicense.startsWith("%%__")) {
             return "pm_" + pmLicense;
+        } else if (!spUser.startsWith("%%__") && !spResource.startsWith("%%__")) {
+            return "sp_" + spResource + "_" + spUser;
         }
         return null;
     }
